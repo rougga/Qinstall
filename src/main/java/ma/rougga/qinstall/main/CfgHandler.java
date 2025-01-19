e5f65baa3b6610b0e1f5cc98229e7f5707d66d93
ROUGGA <bougado.youssef@hotmail.com>
2025-01-19 22:30:34 +0100

    redesigne

---
+++ src/main/java/ma/rougga/qinstall/main/CfgHandler.java
@@ -0,0 +1,35 @@
+package ma.rougga.qinstall.main;
+
+public class CfgHandler {
+    
+    //MetaData
+    public  static final String APP = "QInstall";
+    public static final String VERSION = "7.3";
+    public static final String COMPANY = "ROUGGA";
+    public static final String CLIENT = "Maroc-NST";
+    public static final int APP_PORT=8989;
+    public static final String QSTATE_VERSION = "0.5.2";
+    public static final String QDATA_VERSION = "0.4.2";
+    public static final String QDISPLAY_VERSION = "0.3.2-LITE";
+    
+    
+    public static String[] QDATA_TABLES = {
+        "agence",
+        "cible",
+        "lastupdate",
+        "t_biz_type",
+        "t_login_log",
+        "t_ticket",
+        "t_user",
+        "t_window",
+        "t_window_status",
+        "rougga_task",
+        "rougga_ticket_task",
+        "rougga_zone",
+        "rougga_agence_zone",
+        "rougga_user",
+        "rougga_user_zone", 
+        "rougga_gbl_table"
+    };
+                    
+}
