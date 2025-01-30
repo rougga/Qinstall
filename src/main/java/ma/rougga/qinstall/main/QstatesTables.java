f7c66f1330101eb44e52432feae2e1ecc7bc99e1
ROUGGA <bougado.youssef@hotmail.com>
2025-01-31 00:50:03 +0100

    bug fix

--- src/main/java/ma/rougga/qinstall/main/QstatesTables.java
+++ src/main/java/ma/rougga/qinstall/main/QstatesTables.java
@@ -2,7 +2,7 @@ package ma.rougga.qinstall.main;
 
 public class QstatesTables {
 
-   private static String[] TABLES = {
+    private static String[] TABLES = {
         "rougga_users",
         "rougga_tasks",
         "rougga_ticket_task",
@@ -13,8 +13,8 @@ public class QstatesTables {
     public static String[] getTABLES() {
         return TABLES;
     }
-   
-    public static final String ROUGGA_USERS = "CREATE TABLE rougga_users "
+
+    public static final String ROUGGA_USERS = "CREATE TABLE IF NOT EXISTS rougga_users "
             + "("
             + "    id character varying(40)  NOT NULL,"
             + "    username character varying(255) NOT NULL unique,"
@@ -33,29 +33,47 @@ public class QstatesTables {
             + "'$31$16$3HyoGMKLc4EvcO0ljwLKNY8pclDUAmd7o_oAQUA7J20',"
             + "'adm','admin','admin');";
 
-    public static final String ROUGGA_TASKS = "CREATE TABLE rougga_tasks "
+    public static final String ROUGGA_TASKS = "CREATE TABLE IF NOT EXISTS rougga_tasks "
             + "("
             + "id character varying(40),"
             + "name character varying(255) not null,"
             + "id_service character varying(32)"
             + ");";
 
-    public static final String ROUGGA_TICKET_TASK = "CREATE TABLE rougga_ticket_task"
+    public static final String ROUGGA_TICKET_TASK = "CREATE TABLE IF NOT EXISTS rougga_ticket_task"
             + "("
             + "id_ticket character varying(32) ,"
             + "id_task  character varying(40) ,"
             + "quantity numeric(5,0) default 1"
             + ");";
     public static final String ROUGGA_PARS
-            = "CREATE TABLE rougga_pars ("
+            = "CREATE TABLE IF NOT EXISTS rougga_pars ("
             + "    name character varying(255) NOT NULL, "
             + "    value character varying(255) NOT NULL, "
             + "    CONSTRAINT rougga_pars_pkey PRIMARY KEY (name)"
             + ");";
-    public static final String ROUGGA_TITLES = "CREATE TABLE rougga_titles ("
+    public static final String ROUGGA_TITLES = "CREATE TABLE IF NOT EXISTS rougga_titles ("
             + "type VARCHAR(255) NOT NULL, "
             + "title VARCHAR(255) NOT NULL, "
             + "CONSTRAINT rougga_titles_pkey PRIMARY KEY (type)"
-            + ");";
+            + ");"
+            + "INSERT INTO rougga_titles (type, title) VALUES "
+            + "('gbl', 'Rapport Globale'), "
+            + "('emp', 'Rapport Employée'), "
+            + "('empser', 'Rapport Employée par service'), "
+            + "('gch', 'Rapport Guichet'), "
+            + "('gchserv', 'Rapport Guichet par service'), "
+            + "('gla', 'Grille d''attente'), "
+            + "('glt', 'Grille de traitement'), "
+            + "('apl', 'Appele'), "
+            + "('ndt', 'Nombre de tickets edités'), "
+            + "('ndtt', 'Nombre de tickets traités'), "
+            + "('ndta', 'Nombre de tickets absents'), "
+            + "('ndtsa', 'Nombre de tickets sans affectation'), "
+            + "('cnx', 'Connexions'), "
+            + "('remp', 'Rendement Employés'), "
+            + "('sgch', 'Supervision: Employée-Guichet'), "
+            + "('ser', 'Supervision: Service'), "
+            + "('tch', 'Rapport Tache');";
 
 }
